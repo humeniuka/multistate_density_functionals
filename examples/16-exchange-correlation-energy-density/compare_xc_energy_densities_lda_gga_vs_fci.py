2feae669eaa5496e99728a7e68a6ec2c45b236eb
Alexander Humeniuk <alexander.humeniuk@gmail.com>
2025-06-08 18:21:15 +0200

    plot Becke's 88 GGA exchange density energy for H2

--- examples/16-exchange-correlation-energy-density/compare_xc_energy_densities_lda_gga_vs_fci.py
+++ examples/16-exchange-correlation-energy-density/compare_xc_energy_densities_lda_gga_vs_fci.py
@@ -3,6 +3,8 @@
 """
 compare LDA exchange-correlation energy densities XC(r) computed with the Chachiyo
 correlation and Dirac exchange with the exact values obtained from full CI.
+
+The Becke 88 GGA exchange energy density is also plotted.
 """
 import matplotlib.pyplot as plt
 import numpy
@@ -13,6 +15,8 @@ import pyscf.gto
 from msdft.ElectronRepulsionOperators import LDACorrelationLikeFunctional
 # Dirac exchange
 from msdft.ElectronRepulsionOperators import LDAExchangeLikeFunctional
+# Becke 1988 exchange
+from msdft.ElectronRepulsionOperators import GGABecke88ExchangeLikeFunctional
 from msdft.MultistateMatrixDensity import MultistateMatrixDensityFCI
 
 def compare_xc_energy_densities(mol, nstate=2):
@@ -36,6 +40,8 @@ def compare_xc_energy_densities(mol, nstate=2):
     # LDA functionals for exchange and correlation
     exchange_lda = LDAExchangeLikeFunctional(mol)
     correlation_lda = LDACorrelationLikeFunctional(mol)
+    # GGA functional for exchange
+    exchange_gga = GGABecke88ExchangeLikeFunctional(mol)
 
     # Evaluate XC-energy density XCᵢⱼ(r) = - xᵢⱼ[D](r) + cᵢⱼ[D](r) along the cut
     # ... with the approximate functionals
@@ -44,6 +50,9 @@ def compare_xc_energy_densities(mol, nstate=2):
     xed_lda = exchange_lda.energy_density(msmd, coords)[0,...]
     ced_lda = correlation_lda.energy_density(msmd, coords)[0,...]
     xced_lda = -xed_lda + ced_lda
+    # GGA exchange-energy summed over spins
+    xed_gga = numpy.sum(exchange_gga.energy_density(msmd, coords), axis=0)
+    xced_gga = -xed_gga + ced_lda
 
     # ... and exactly using the pair-density.
     xced_fci = msmd.exchange_correlation_energy_density(coords)
@@ -84,20 +93,32 @@ def compare_xc_energy_densities(mol, nstate=2):
                 label=label)
 
             # approximate exchange
+            # ... LDA
             axes[0,column].plot(
                 r, -xed_lda[istate,jstate,:],
                 lw=1, color=line.get_color(),
                 ls='--', label=r"x$_{%d,%d}(r)$ (LDA)" % (istate, jstate))
+            # ... GGA (Becke 88)
+            axes[0,column].plot(
+                r, -xed_gga[istate,jstate,:],
+                lw=1, color=line.get_color(),
+                ls=':', label=r"x$_{%d,%d}(r)$ (GGA)" % (istate, jstate))
             # approximate correlation
             axes[1,column].plot(
                 r, ced_lda[istate,jstate,:],
                 lw=1, color=line.get_color(),
                 ls="-.", label=r"c$_{%d,%d}(r)$ (LDA)" % (istate, jstate))
             # approximate exchange-correlation energy density
+            # .. LDA exchange + LDA correlation
             axes[2,column].plot(
                 r, xced_lda[istate,jstate,:],
                 lw=1, color=line.get_color(),
                 ls="-.", label=r"xc$_{%d,%d}(r)$ (LDA)" % (istate, jstate))
+            # ... GGA exchange + LDA correlation
+            axes[2,column].plot(
+                r, xced_gga[istate,jstate,:],
+                lw=1, color=line.get_color(),
+                ls=":", label=r"xc$_{%d,%d}(r)$ (GGA+LDA)" % (istate, jstate))
 
     for row in [0,1,2]:
         for column in [0,1]:
@@ -109,7 +130,7 @@ if __name__ == "__main__":
     # hydrogen molecule, closed shell
     mol = pyscf.gto.M(
         atom = 'H 0 0 -0.35; H 0 0 0.35',
-        basis = '6-31g', #'cc-pvdz',
+        basis = 'aug-cc-pvtz',
         # singlet
         spin = 0)
 
