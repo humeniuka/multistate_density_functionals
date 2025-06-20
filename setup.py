b30955c25117978e4dae8b02c9830ddcb811a9a9
Alexander Humeniuk <alexander.humeniuk@gmail.com>
2025-06-20 17:50:25 +0200

    pyscf version is set in setup.py not in environment.yaml

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     author_email='alexander.humeniuk@gmail.com',
     license='LICENSE.txt',
     package_dir = {"": "src"},
-    install_requires=['becke-multicenter-integration', 'matplotlib', 'numpy<2.0', 'pyscf==2.4.0', 'scipy', 'tqdm'],
+    install_requires=['becke-multicenter-integration', 'matplotlib', 'numpy<2.0', 'pyscf==2.9.0', 'scipy', 'tqdm'],
     include_package_data=True,
     zip_safe=False,
 )
